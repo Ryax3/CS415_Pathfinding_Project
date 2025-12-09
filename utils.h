95779b4ba9a2d3628afd47582f7ff5a79703db03
Rolando Yax <yaxsayr@sonoma.edu>
2025-12-09 13:05:21 -0800

    Final update: Robust BMP loader, updated search algorithms, utilities, and maze generator for full instructor compliance

--- utils.h
+++ utils.h
@@ -1,31 +1,3 @@
-
-// #ifndef UTILS_H
-// #define UTILS_H
-
-// #include "bmp.h"
-// #include <vector>
-// #include <cstdint>
-
-// struct Pixel
-// {
-//     int row;
-//     int col;
-// };
-
-// bool in_bounds(const BMPImage &img, int r, int c);
-// bool is_vertex(const BMPImage &img, int r, int c);
-// int manhattan(const Pixel &a, const Pixel &b);
-
-// void color_visited_and_path(
-//     BMPImage &img,
-//     const std::vector<std::vector<bool>> &visited,
-//     const std::vector<std::vector<Pixel>> &prev,
-//     const Pixel &start,
-//     const Pixel &goal,
-//     bool reached);
-
-// #endif
-
 #ifndef UTILS_H
 #define UTILS_H
 
